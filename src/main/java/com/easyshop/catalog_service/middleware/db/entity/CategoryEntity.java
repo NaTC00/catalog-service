fc529771b90909cabccab43f3e6ff51a510c4559
Natalia Ceccarini <n.ceccarini@intouch-srl.com>
2025-11-17 14:54:28 +0100

    add the class that maps the database table and products

---
+++ src/main/java/com/easyshop/catalog_service/middleware/db/entity/CategoryEntity.java
@@ -0,0 +1,14 @@
+package com.easyshop.catalog_service.middleware.db.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@ToString
+@Getter
+public enum CategoryEntity {
+    LAPTOP("laptop"),
+    SMARTPHONE("smartphone");
+    private final String value;
+}
---
+++ src/main/java/com/easyshop/catalog_service/middleware/db/entity/ProductEntity.java
@@ -0,0 +1,14 @@
+package com.easyshop.catalog_service.middleware.db.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table(name="products")
+public record ProductEntity (
+    @Id Long productId,
+    String code,
+    String name,
+    Long price,
+    String brand,
+    CategoryEntity category
+) {}
